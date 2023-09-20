6ede3cc20d0f8b246b1220514756ed9e5f427a89
Jovhor <dr.jovhor@mail.ru>
2023-09-20 22:29:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Odd Even Numbers \n");
+
+            Console.WriteLine("Приложение для определения четности числа");
+
+            for (; ; )
+            {
+                Console.WriteLine("Введите число:");
+                int userNumber = int.Parse(Console.ReadLine());
+
+                if (userNumber % 2 == 0)
+                    {
+                        Console.WriteLine("Четное");
+                    }
+                else
+                {
+                    Console.WriteLine("Нечетное");
+                }
+            }
+        }
+    }
+}
